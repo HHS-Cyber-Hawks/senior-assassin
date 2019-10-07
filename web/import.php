c68d5c403f2f361c9107b3a3c87ff69d647f92bc
Peter Bell <pbell20@hhs-106-017.hanoverstudents.org>
2019-10-07 09:46:49 -0400

    Display CSV of Class-of-2020

---
+++ web/import.php
@@ -0,0 +1,29 @@
+
+<?php
+$servername = "mysql.server295.com";
+$username = "assassin";
+$password = "billiard gale seeing";
+$dbname = "passingf_assassin";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$row = 1;
+if (($handle = fopen("class-of-2020.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        echo "<p> $num fields in line $row: <br /></p>\n";
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+            echo $data[$c] . "<br />\n";
+        }
+    }
+    fclose($handle);
+}
+?>
