4d0fe27e4df600912d8eafba15e0c67e5079a6b6
lucas-bertoni <lbertoni1227@hotmail.com>
2019-10-07 09:20:42 -0400

    Minor changes

--- web/assignment_create.php
+++ web/assignment_create.php
@@ -13,14 +13,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = <<<SQL
-          SELECT player_id
-          FROM num_players
-          ORDER BY player_id
-          ;
-SQL;
-
-
 $get_count = <<<SQL
                 SELECT count(player_id) as num_players
                 FROM players
@@ -31,16 +23,6 @@ $result = $conn->query($get_count);
 $row = $result->fetch_assoc();
 
 $num_players = $row["num_players"];
-
-for($i = 1; $i <= $num_players; $i++) {
-    $make_assignment = "INSERT INTO assignments(attacker_id, victim_id, the_status) " .
-                       "VALUES(" . ($i + 1) . ", " . ($num_players - $i) . ", 0);";
-    $conn->query($make_assignment);
-    echo "Created <br/>";
-}
-
-
-
 
 $conn->close();
 
