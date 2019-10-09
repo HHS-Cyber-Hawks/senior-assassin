6b676cc9aa37c3c122a9a3bd1e2379fb56875f15
lucas-bertoni <lbertoni1227@hotmail.com>
2019-10-09 09:43:28 -0400

    Messing with assignment_create

--- web/assignment_create.php
+++ web/assignment_create.php
@@ -1,3 +1,8 @@
+
+<html>
+  <body>
+
+
 <?php
 
 $servername = "mysql.server295.com";
@@ -13,6 +18,27 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Gets rid of the assignments table if it currently exists
+$drop_table = <<<SQL
+                 DROP TABLE IF EXISTS assignments;
+SQL;
+
+$conn->query($drop_table);
+
+// Recreates the assignments table with no values inside of it
+$make_table = <<<SQL
+                  CREATE TABLE assignments (
+                  assignment_id  INT NOT NULL AUTO_INCREMENT,
+                  attacker_id    INT NOT NULL,
+                  target_id      INT NOT NULL,
+                  status         INT default 0,
+                  PRIMARY KEY (assignment_id))
+                  ;
+SQL;
+
+$conn->query($make_table);
+
+
 // Queries database for the amount of player ids there are
 $get_count = <<<SQL
                 SELECT count(player_id) as num_players
@@ -20,6 +46,7 @@ $get_count = <<<SQL
                 ;
 SQL;
 
+// Gets results from query
 $result = $conn->query($get_count);
 $row = $result->fetch_assoc();
 
@@ -33,36 +60,64 @@ for($i = 1; $i <= $num_players; $i++)
     array_push($attacker_array, $i);
 }
 
-// Makes the victim array and sets it equal to the attacker array
-$victim_array = (new ArrayObject($attacker_array))->getArrayCopy();
-// Shuffles the victim array
-shuffle($victim_array);
-// Checks to make sure no attacker has themself as a target. If this does occur, the value is switched
-checkArray($attacker_array, $victim_array, $num_players);
+$target_array = array();
+$rand_num_array = array();
 
-// Checks all indices of both matrices and makes sure they aren't the same. Swaps values in victim_array if they are
-function checkArray($x, &$y, $num_players)
+while(sizeof($target_array) < $num_players)
 {
-  for($i = 0; $i < $num_players; $i++)
+  // to push
+  $rand_num = rand(0, $num_players - 1);
+
+  if(!contains($rand_num_array, $rand_num))
   {
-    if($x[$i] == $y[$i])
+    array_push($target_array, $rand_num);
+    array_push($rand_num_array, $rand_num);
+  }
+}
+
+var_dump($target_array);
+
+
+
+function contains(&$array, $to_check)
+{
+  for ($i=0; $i < sizeof($array); $i++) {
+    if($array[$i] == $to_check)
     {
-      if($i == $num_players - 1)
-      {
-        $temp = $y[$i];
-        $y[$i] = $y[$i - 1];
-        $y[$i - 1] = $temp;
-      }
-      else
-      {
-        $temp = $y[$i];
-        $y[$i] = $y[$i + 1];
-        $y[$i + 1] = $temp;
-      }
+      return true;
     }
   }
+  return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+// Pushes the pairings into the assignments table
+for($i = 0; $i < $num_players; $i++)
+{
+  $sql = <<<SQL
+            INSERT INTO assignments (attacker_id, target_id, status)
+            VALUES ('$attacker_array[$i]', '$target_array[$i]', 0);
+SQL;
+
+  $conn->query($sql);
 }
 
 $conn->close();
 
+//header('Location: assignment_display.php');
+
 ?>
+
+
+
+  </body>
+</html>
--- web/assignment_display.php
+++ web/assignment_display.php
@@ -4,7 +4,11 @@
     <script src='scripts.js?<?php echo rand(); ?>'></script>
     <link rel="stylesheet" type="text/css" href="styles.css?<?php echo rand(); ?>" />
   </head>
-
+  <body>
+    <div class="header">
+      <a href="assignment_create.php"><button class="button" style="height: auto; margins: auto;">Create Assignments</button></a>
+    </div>
+    <br />
 <?php
 
 $servername = "mysql.server295.com";
@@ -24,12 +28,12 @@ $sql = <<<SQL
           SELECT assignment_id,
           attackers.first_name as attacker_first_name,
           attackers.last_name as attacker_last_name,
-          victims.first_name as victim_first_name,
-          victims.last_name as victim_last_name,
-          the_status
+          targets.first_name as target_first_name,
+          targets.last_name as target_last_name,
+          status
           FROM assignments
           JOIN players attackers ON attackers.player_id = attacker_id
-          JOIN players victims ON victims.player_id = victim_id
+          JOIN players targets ON targets.player_id = target_id
           ;
 SQL;
 
@@ -45,8 +49,8 @@ if ($result->num_rows > 0) {
         echo "<tr>";
         echo "<td>" . $row["assignment_id"]      . "</td>";
         echo "<td>" . $row["attacker_first_name"] . " " . $row["attacker_last_name"] . "</td>";
-        echo "<td>" . $row["victim_first_name"]   . " " . $row["victim_last_name"]   . "</td>";
-        echo "<td>" . $row["the_status"]          . "</td>";
+        echo "<td>" . $row["target_first_name"]   . " " . $row["target_last_name"]   . "</td>";
+        echo "<td>" . $row["status"]          . "</td>";
         echo "</tr>";
     }
 
@@ -58,5 +62,5 @@ if ($result->num_rows > 0) {
 
 $conn->close();
 ?>
-
+  </body>
 </html>
