d7e2df2d61c7a9c35502133874585f392bc766e5
PeterNBell <32136284+PeterNBell@users.noreply.github.com>
2019-12-17 09:15:33 -0500

    Only show Confirmed kills in history

--- web/player_stats.php
+++ web/player_stats.php
@@ -18,7 +18,7 @@ $sql = <<<SQL
           FROM assignments
           JOIN players attackers ON attackers.player_id = attacker_id
           JOIN players targets ON targets.player_id = target_id
-          WHERE (attacker_id = $id OR target_id = $id) AND assignment_status <> 0
+          WHERE (attacker_id = $id OR target_id = $id) AND (assignment_status = 2)
           ORDER BY assignment_round;
 SQL;
 $result = $conn->query($sql);
